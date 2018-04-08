0f205b98343242f45931f9181da7886ceaabdaf2
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-04-08 01:03:11 -0700

    todo rendering

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [
+        { desc: 'Walk the cat', isCompleted: true },
+        { desc: 'Throw the dishes away', isCompleted: false },
+        { desc: 'Buy new dishes', isCompleted: false}
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.desc} isCompleted={ todo.isCompleted } />
+          )}
         </ul>
       </div>
     );
