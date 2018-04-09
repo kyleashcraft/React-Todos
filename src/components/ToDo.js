a4a8e39122c23fb8ec85c8b5bee2cd2f00d60b41
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-04-08 22:55:56 -0700

    delete todos

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,7 +5,7 @@ class ToDo extends Component {
     return (
       <li>
       <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-        { this.props.description }
+        { this.props.description } <button onClick={ this.props.deleteTodo } >Delete</button>
       </li>
     );
   }
